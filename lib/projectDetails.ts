ec50c67858e923b481f44991f920c3d2b8ebd5dc
subhajitsarkar-dev <subhajitsarkar.dev@gmail.com>
2025-09-15 00:34:14 +0530

    Modifi the position of project

--- lib/projectDetails.ts
+++ lib/projectDetails.ts
@@ -1,17 +1,6 @@
 export const projectInfo: ProjectInfo[] = [
   {
     id: 1,
-    title: "Edu Meeting",
-    descriptions:
-      "A responsive Education Meeting UI designed to enhance virtual learning experiences.",
-    techStack: "Html5, Css3, Boostrap, Font Awesome",
-    liveDemo:
-      "https://education-tau-seven.vercel.app/index.html#[object%20Object]",
-    github: "https://github.com/subhajitsarkar-dev/Education",
-    badge: "Frontend",
-  },
-  {
-    id: 2,
     title: "Kanban Board",
     descriptions:
       "Kanban Board is a simple task manager to organize tasks into To Do, Doing, and Done columns.",
@@ -21,13 +10,24 @@ export const projectInfo: ProjectInfo[] = [
     badge: "Frontent",
   },
   {
-    id: 3,
+    id: 2,
     title: "Weather App",
     descriptions:
       "A responsive weather app showing real-time conditions, 3-day forecasts, and hourly updates based on your location.",
     techStack: "NextJs, TypeScript, Tailwind",
     liveDemo: "https://weather-application-se5x.vercel.app/",
     github: "https://github.com/subhajitsarkar-dev/Weather-Application",
+    badge: "Frontend",
+  },
+  {
+    id: 3,
+    title: "Edu Meeting",
+    descriptions:
+      "A responsive Education Meeting UI designed to enhance virtual learning experiences.",
+    techStack: "Html5, Css3, Boostrap, Font Awesome",
+    liveDemo:
+      "https://education-tau-seven.vercel.app/index.html#[object%20Object]",
+    github: "https://github.com/subhajitsarkar-dev/Education",
     badge: "Frontend",
   },
   {
